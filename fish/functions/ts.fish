c83473e058ad966493a47ec95969152401f9b4f3
Snow <snowmann0@proton.me>
2026-05-12 06:57:38 +0300

    sync | freezer | updated new files

--- fish/functions/ts.fish
+++
@@ -1,8 +0,0 @@
-########################################################################
-##  SnowDots — ts                             Version: v1.0.0    ##
-##  Last Edited: 2026-05-05                                           ##
-########################################################################
-
-function ts --wraps=tailscale --description 'alias ts=tailscale'
-    tailscale $argv
-end
