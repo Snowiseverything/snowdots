125fd01197350a6b791f28ffa812d4441bbbbdab
Snow <snow@snowpi>
2026-05-16 09:34:27 +0300

    snowpi: fish config, uv env, ts-check functions

--- fish/functions/tscheck.fish
+++ fish/functions/tscheck.fish
@@ -1,8 +1,4 @@
-########################################################################
-##  SnowDots — tscheck                             Version: v1.0.0    ##
-##  Last Edited: 2026-05-05                                           ##
-########################################################################
-
-function tscheck --wraps='~/Dotfiles/scripts/ts-check.sh' --description 'alias tscheck=~/Dotfiles/scripts/ts-check.sh'
-    ~/Dotfiles/scripts/ts-check.sh $argv
+function tscheck --wraps='~/SnowPi-Dotfiles/scripts/ts-check.sh' --description 'alias tscheck=~/SnowPi-Dotfiles/scripts/ts-check.sh'
+  ~/SnowPi-Dotfiles/scripts/ts-check.sh $argv
+        
 end
