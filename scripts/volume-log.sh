1b97c696ebbb302fd2f5d73f7f85dac50d2bacb9
Snow <snowmann0@proton.me>
2026-05-20 16:36:02 +0300

    feat: volume cap at 100%, toggle unlimited with Super+Shift+V

--- scripts/volume-log.sh
+++ scripts/volume-log.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 ACTION="$1"
 SINK="@DEFAULT_AUDIO_SINK@"
+LIMIT=""
+
+if [ ! -f /tmp/volume-unlimited ]; then
+    LIMIT="--limit 1.0"
+fi
+
 CURRENT=$(wpctl get-volume $SINK | awk '{print int($2 * 100)}')
 
 if [ "$CURRENT" -lt 15 ]; then STEP=2
@@ -10,4 +16,4 @@ elif [ "$CURRENT" -lt 85 ]; then STEP=8
 else STEP=10
 fi
 
-wpctl set-volume $SINK ${STEP}%${ACTION}
+wpctl set-volume $LIMIT $SINK ${STEP}%${ACTION}
