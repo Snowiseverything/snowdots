6b896ba8036ac765d0b3e3d8c027827d55c49bf2
Snow <snowmann0@proton.me>
2026-05-20 13:20:21 +0300

    fix: include hidden dirs, exclude node_modules in fzf commands

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -8,9 +8,17 @@ set -gx FZF_CTRL_T_OPTS "--prompt=[files]"
 set -gx FZF_CTRL_R_OPTS "--prompt=[history] --reverse"
 set -gx FZF_ALT_C_OPTS "--prompt=[dirs] --scheme=default --tiebreak=begin --preview=fzf_preview_dir --preview-window=right:50%"
 
-# Alt+C: list subdirs + .. (go up), preview dir contents
+# Ignore noise dirs
+set -gx FZF_DEFAULT_COMMAND "
+  command find . -not -path './node_modules/*' -not -path './.git/*' -not -path './.cache/*' 2>/dev/null | sed 's|^\./||'
+"
+set -gx FZF_CTRL_T_COMMAND "
+  command find . -not -path './node_modules/*' -not -path './.git/*' -not -path './.cache/*' -type f 2>/dev/null | sed 's|^\./||'
+"
+
+# Alt+C: list subdirs (incl hidden) + .. (go up), preview dir contents
 set -gx FZF_ALT_C_COMMAND "
-  command find . -mindepth 1 -maxdepth 5 -type d 2>/dev/null | sed 's|^\\./||'
+  command find . -mindepth 1 -maxdepth 5 -not -path './node_modules/*' -not -path './.git/*' -not -path './.cache/*' -type d 2>/dev/null | sed 's|^\./||'
   echo ..
 "
 
