e8e4bf568d2c2426eb8b004d016dfa64e4f983ce
Snow <snowmann0@proton.me>
2026-05-22 01:08:15 +0300

    public: sanitized release 2026-05-22

--- fish/functions/ssh.fish
+++ fish/functions/ssh.fish
@@ -4,7 +4,7 @@
 ########################################################################
 
 function ssh
-    if string match -q "*snowpi*" -- $argv; or string match -q "*192.168.1.35*" -- $argv
+    if string match -q "*snowpi*" -- $argv; or string match -q "*192.168.1.200*" -- $argv
         kitty --detach sh -c "exec ssh -t $argv fish"
     else
         command ssh $argv
